9a5efbba226a1b73c4cd159d5e6ac3c25dc6f942
Алексей <would_be_happy@mail.ru>
2023-08-31 19:26:23 +0300

    dz1.py

---
+++ dz1.py
@@ -0,0 +1,16 @@
+polind def(полиндром):
+    полиндром ==полиндромесли[::-1]:
+        печать('True')
+    полиндром !=полиндромесли[::-1]:
+        печать('False')
+
+полинд("привет")
+polind('dwdwdfe')
+def polind(polindrom):
+    if polindrom==polindrom[::-1]:
+        print('True')
+    if polindrom!=polindrom[::-1]:
+        print('False')
+
+polind('helleh')
+polind('dwdwdfe')
